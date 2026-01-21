0bbbd2661cff7f797ab866ceaf1edc129201df1c
Utkarsh Shukla <96052149+utkrsh-shukla@users.noreply.github.com>
2026-01-21 14:10:23 +0530

    Change CORS_ORIGINS to allow all origins
    Updated CORS_ORIGINS to allow all origins for testing.

--- backend/app/config.py
+++ backend/app/config.py
@@ -34,7 +34,8 @@ class Config:
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'noreply@hospital.com'
     
     # CORS Configuration
-    CORS_ORIGINS = ['http://localhost:8080', 'http://localhost:5173', 'https://patch-me-up-version2-an-hms-system.vercel.app/api']  # VueJS dev servers
+    #CORS_ORIGINS = ['http://localhost:8080', 'http://localhost:5173', 'https://patch-me-up-version2-an-hms-system.vercel.app/api']  # VueJS dev servers
+    CORS_ORIGINS = ['*']         # TESTING CONNECTIONS
 
 class DevelopmentConfig(Config):
     """Development configuration"""
